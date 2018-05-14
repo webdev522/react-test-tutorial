baf264c73826b47d1b8ab5412b64fffb08ea1818
alexiilab <labhrov1@mail.ru>
2018-05-14 10:57:19 -0700

    enzyme

--- src/components/NameForm.test.js
+++ src/components/NameForm.test.js
@@ -1,16 +1,48 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import NameForm from "./NameForm";
 
 describe("NameForm", () => {
   let component = null;
 
+  let changed = null;
+  const onInsert = name => {
+    changed = name;
+  };
+
   it("renders correctly", () => {
-    component = renderer.create(<NameForm />);
+    component = shallow(<NameForm onInsert={onInsert} />);
   });
 
   it("matches snapshot", () => {
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    // const tree = component.toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  describe("insert new text", () => {
+    it("has a form", () => {
+      expect(component.find("form").exists()).toBe(true);
+    });
+    it("has an input", () => {
+      expect(component.find("input").exists()).toBe(true);
+    });
+    it("simulates input change", () => {
+      const mockedEvent = {
+        target: {
+          value: "hello"
+        }
+      };
+      // simulate event. the second param is an event obj
+      component.find("input").simulate("change", mockedEvent);
+      expect(component.state().name).toBe("hello");
+    });
+    it("simulates form submit", () => {
+      const mockedEvent = {
+        preventDefault: () => null
+      };
+      component.find("form").simulate("submit", mockedEvent);
+      expect(component.state().name).toBe("");
+      expect(changed).toBe("hello");
+    });
   });
 });
