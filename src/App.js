e654b8ef052025f486658c50c208c505e9148008
alexiilab <labhrov1@mail.ru>
2018-05-14 23:46:00 -0700

    redux applied

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
-import Counter from "./components/Counter";
-import NameForm from "./components/NameForm";
-import NameList from "./components/NameList";
+// import Counter from "./components/Counter";
+// import NameForm from "./components/NameForm";
+// import NameList from "./components/NameList";
+import CounterContainer from "./containers/CounterContainer";
+import NamesContainer from "./containers/NamesContainer";
+import PostContainer from "./containers/PostContainer";
 
 class App extends Component {
-  state = {
-    names: ["webdev522", "Web Dev"]
-  };
+  // state = {
+  //   names: ["webdev522", "Web Dev"]
+  // };
 
-  onInsert = name => {
-    this.setState(({ names }) => ({ names: names.concat(name) }));
-  };
+  // onInsert = name => {
+  //   this.setState(({ names }) => ({ names: names.concat(name) }));
+  // };
 
   render() {
-    const { names } = this.state;
-    const { onInsert } = this;
+    // const { names } = this.state;
+    // const { onInsert } = this;
 
     return (
       <div>
-        <Counter />
+        <CounterContainer />
         <hr />
         <h1>Name List</h1>
-        <NameForm onInsert={onInsert} />
-        <NameList names={names} />
+        <NamesContainer />
+        <hr />
+        <PostContainer />
       </div>
     );
   }
--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -2,33 +2,17 @@ import React, { Component } from "react";
 
 class NameForm extends Component {
   static defaultProps = {
-    onSubmit: () => console.warn("onSubmit not defined")
+    onSubmit: () => console.warn("onSubmit not defined"),
+    onChange: () => console.warn("onChange not defined"),
+    value: ""
   };
-  state = {
-    name: ""
-  };
-  onChange = e => {
-    this.setState({
-      name: e.target.value
-    });
-  };
-  onSubmit = e => {
-    const { name } = this.state;
-    const { onInsert } = this.props;
 
-    onInsert(name);
-    this.setState({
-      name: ""
-    });
-    e.preventDefault();
-  };
   render() {
-    const { onSubmit, onChange } = this;
-    const { name } = this.state;
+    const { onSubmit, onChange, value } = this.props;
     return (
       <form onSubmit={onSubmit}>
         <label>Name</label>
-        <input type="text" value={name} onChange={onChange} />
+        <input type="text" value={value} onChange={onChange} />
         <button type="submit">Register</button>
       </form>
     );
